325235ebc61b986562bc851f1a36532bf1ff1856
Kojegar <102160249+Kojegar@users.noreply.github.com>
2022-05-24 23:48:27 +0800

    first commit

---
+++ app/src/main/java/com/example/bober/nonavto.java
@@ -0,0 +1,14 @@
+package com.example.bober;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class nonavto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nonavto);
+    }
+}
